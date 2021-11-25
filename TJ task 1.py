fa8ba9db47a677d6ce9c21246b5b9140e8e246dc
GauravSingh2004 <95028979+GauravSingh2004@users.noreply.github.com>
2021-11-25 17:22:40 +0530

    Add files via upload

---
+++ TJ task 1.py
@@ -0,0 +1,19 @@
+sample_url=[]
+x = 'http://google.com'
+sample_url.append(x)
+for i in sample_url:
+    print('The entered url is :- ',i)
+    print('')
+    print('Reversing the given url :- ',i[::-1])
+    print('')
+    print('The top level domain is :- ',i[13:17])
+    print('')
+    print('The url withot the http: is :- ',i[5:17])
+    print('')
+    print('the url without the http:// or the top level domain is :- ',i[7:13])
+
+
+
+
+
+    
